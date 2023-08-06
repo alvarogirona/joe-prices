983b400572a45af33c6a3554ccf9d7e1f36a5ec1
Álvaro <alvaro.girona@gmail.com>
2023-08-06 05:43:23 +0200

    Add network helper functions

--- apps/joe_prices/lib/joe_prices/core/network.ex
+++ apps/joe_prices/lib/joe_prices/core/network.ex
@@ -4,4 +4,10 @@ defmodule JoePrices.Core.Network do
   def get_rpc_from_network(:arbitrum_mainnet), do: "https://rpc.ankr.com/arbitrum"
   def get_rpc_from_network(:avalanche_mainnet), do: "https://rpc.ankr.com/avalanche"
   def get_rpc_from_network(:bsc_mainnet), do: "https://rpc.ankr.com/bsc"
+
+  def arbitrum_mainnet(), do: :arbitrum_mainnet
+  def avalanche_mainnet(), do: :avalanche_mainnet
+  def bsc_mainnet(), do: :bsc_mainnnet
+
+  def all_networks(), do: [arbitrum_mainnet(), avalanche_mainnet(), bsc_mainnet()]
 end
