8f6b3c38e47b332aa113994f8b150fcd29ba1ddb
Álvaro <alvaro.girona@gmail.com>
2023-08-06 02:55:03 +0200

    Make calls to LbFactory with configurable rpc based on network

--- apps/joe_prices/lib/joe_prices/contracts/v21/lb_factory.ex
+++ apps/joe_prices/lib/joe_prices/contracts/v21/lb_factory.ex
@@ -2,16 +2,36 @@ defmodule JoePrices.Contracts.V21.LbFactory do
   alias JoePrices.Utils.Parallel
   import Parallel
 
+  alias JoePrices.Core.Network
+
+  @type network_name() :: :arbitrum_mainnet | :avalanche_mainnet | :bsc_mainnet
+
+  @doc """
+  ## Example
+
+    JoePrices.Contracts.V21.LbFactory.fetch_pairs({:arbitrum_mainnet, "0x8e42f2F4101563bF679975178e880FD87d3eFd4e"})
+
+  """
   use Ethers.Contract,
     abi_file: "priv/abis/v21/LBFactory.json",
     default_address: "0x8e42f2F4101563bF679975178e880FD87d3eFd4e"
 
-    def fetch_pairs({_network, address} = name) do
-      [pairs_count] = __MODULE__.get_number_of_lb_pairs!(to: address)
+  @doc """
+  Calls contract to fetch all available pairs.
+  """
+  def fetch_pairs(network) do
+    network_rpc = Network.get_rpc_from_network(network)
+    contract_address = contract_for_network(network)
 
-      0..pairs_count
-      |> Enum.to_list
-      |> pmap(&__MODULE__.get_lb_pair_at_index(&1))
-      |> Enum.filter
-    end
+    [pairs_count] = __MODULE__.get_number_of_lb_pairs!(to: contract_address, rpc_opts: [{:url, network_rpc}])
+
+    responses = 0..pairs_count
+    |> Enum.to_list
+    |> pmap(&__MODULE__.get_lb_pair_at_index(&1))
+
+    ok_responses = responses
+      |> Enum.filter(&match?({:ok, _}, &1))
+  end
+
+  defp contract_for_network(_), do: "0x8e42f2F4101563bF679975178e880FD87d3eFd4e"
 end
