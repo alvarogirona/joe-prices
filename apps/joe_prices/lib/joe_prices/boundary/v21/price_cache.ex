ec65ec20b177ee89745d003499dc8aac43323d23
Álvaro <alvaro.girona@gmail.com>
2023-08-06 05:43:34 +0200

    Price cache implementation

--- apps/joe_prices/lib/joe_prices/boundary/v21/price_cache.ex
+++ apps/joe_prices/lib/joe_prices/boundary/v21/price_cache.ex
@@ -1,20 +1,66 @@
 defmodule JoePrices.Boundary.V21.PriceCache do
   use GenServer
 
+  @ttl 60
+  @ets_table_suffix :prices_cache_v21
+
+  @type network_name :: :arbitrum_mainnet | :avalanche_mainnet | :bsc_mainnet
+
+  @moduledoc """
+  Cache for prices.
+
+  ## Structure of the cache
+  ```
+    %{
+      :updated_at => TIMESTAMP
+      :items => {:key => CACHE_ITEM}
+    }
+  ```
+
+  Key format:
+  ```
+  token_x_address-token_y_address
+  ```
+  * Sorted alphabetically
+
+  `CACHE_ITEM`:
+  ```
+    %{
+      token_x_address: String,
+      token_y_address: String,
+      active_bin: Int,
+      bin_step: Int,
+    }
+  ```
+  """
+
   import JoePrices.Utils.Parallel
 
   alias JoePrices.Contracts.V21.LbFactory, as: LBFactoryContract
 
-  def init(opts) do
-    {:ok, opts}
+  def child_spec([network: network] = opts) do
+    %{
+      id: network,
+      start: {__MODULE__, :start_link, opts},
+      shutdown: 30_000,
+      restart: :permanent,
+      type: :worker
+    }
   end
 
-  @spec start_link({:arb | :avax | :bsc, String}) :: {:ok, term}
-  def start_link({network, address} = config) do
+  def init({network}) do
+    create_ets_table(network)
+
+    {:ok, {network}}
+  end
+
+  # @spec start_link({@network_name}) :: {:ok, term}
+  def start_link({:network, network}) do
+    IO.inspect(network)
     GenServer.start_link(
       __MODULE__,
-      {network, address},
-      name: via(config),
+      {network},
+      name: via(network)
     )
   end
 
@@ -22,21 +68,52 @@ defmodule JoePrices.Boundary.V21.PriceCache do
     Returns all the available pairs from a cache.
 
     If the cache was expired, data is renewed before returning it.
+
+    ## Example
+
+    iex> JoePrices.Boundary.V21.PriceCache.get_price({1,2})
   """
-  def get_all_pairs({network, address} = name) do
-    GenServer.call(via(name), {:get_all_pairs, name})
+  def get_price(network, {_token_x, _token_y} = tokens) do
+    GenServer.call(via(network), {:get_all_pairs, tokens})
   end
 
   # GenServer Handlers
-  def handle_call({:get_all_pairs, address} = name) do
-    all_pairs = LBFactoryContract.get_all_pairs(name)
+  def handle_call({:get_all_pairs, {token_x, token_y}}, _from, {network}) do
+    IO.puts(">>>>>")
+    all_pairs = LBFactoryContract.fetch_pairs(network)
+    IO.inspect(all_pairs)
+
+    table_name = get_table_name(network)
+    IO.inspect(table_name)
+
+    :ets.insert(table_name, all_pairs)
   end
 
-  defp via({_network, _address} = name) do
+  @doc """
+  Function to be scheduled when the cache is refreshed to refresh it in the future.
+
+  It will take a key and invalidate its contents.
+  """
+  def handle_info({:invalidate, key}, state) do
+    :ets.delete(@ets_table, key)
+    {:noreply, state}
+  end
+
+  defp create_ets_table(network) when is_atom(network) do
+    table_name = get_table_name(network)
+    :ets.new(table_name, [:set, :protected, :named_table])
+  end
+
+  defp get_table_name(network) when is_atom(network) do
+    (Atom.to_string(network) <> Atom.to_string(@ets_table_suffix))
+    |> String.to_atom
+  end
+
+  defp via(network) do
     {
       :via,
       Registry,
-      {JoePrices.Registry.V21.LBFactory, name},
+      {JoePrices.Registry.V21.PriceCache, network},
     }
   end
 end
