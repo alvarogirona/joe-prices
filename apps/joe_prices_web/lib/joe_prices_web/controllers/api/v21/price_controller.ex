5d803a19873c4075d52d7386000b992d690f0aaa
alvarogirona <alvaro.girona@gmail.com>
2023-08-16 15:33:50 +0200

    Create render func and render multiple prices for v21

--- apps/joe_prices_web/lib/joe_prices_web/controllers/api/v21/price_controller.ex
+++ apps/joe_prices_web/lib/joe_prices_web/controllers/api/v21/price_controller.ex
@@ -1,6 +1,7 @@
 defmodule JoePricesWeb.Api.V21.PriceController do
   use JoePricesWeb, :controller
 
+  alias JoePrices.Boundary.V21.Cache.PriceCacheEntry
   alias JoePrices.Boundary.V21.PriceRequest
 
   def index(conn, opts) do
@@ -16,18 +17,17 @@ defmodule JoePricesWeb.Api.V21.PriceController do
 
     price = JoePricesV21.get_price(price_request)
 
-    json(conn, %{
-      token_x: price.token_x,
-      token_y: price.token_y,
-      price: JoePrices.Core.V21.Bin.get_price_from_id(price.active_bin, price.bin_step)
-    })
+    json(conn, render_price(price))
   end
 
   def batch(conn, %{"tokens" => tokens_list} = opts) do
     parsed_tokens = tokens_list
     |> Enum.map(&parse_token_request/1)
 
-    text(conn, "batch")
+    prices = JoePricesV21.get_prices(parsed_tokens)
+    |> render_prices()
+
+    json(conn, prices)
   end
 
   defp parse_token_request(%{"token_x" => tx, "token_y" => ty, "bin_step" => bs} = token_request) do
@@ -35,6 +35,18 @@ defmodule JoePricesWeb.Api.V21.PriceController do
       token_x: tx,
       token_y: ty,
       bin_step: bs
+    }
+  end
+
+  defp render_prices(prices) do
+    Enum.map(prices, &render_price/1)
+  end
+
+  defp render_price(price) do
+    %{
+      token_x: price.token_x,
+      token_y: price.token_y,
+      price: JoePrices.Core.V21.Bin.get_price_from_id(price.active_bin, price.bin_step)
     }
   end
 end
