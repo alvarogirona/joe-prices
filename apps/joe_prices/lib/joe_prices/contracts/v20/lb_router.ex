5b1db748a888d4e4b422685c125be2ecc0cb3a4c
alvarogirona <alvaro.girona@gmail.com>
2023-08-18 23:08:24 +0200

    Make contract_for_network private on v2.0 LbRouter

--- apps/joe_prices/lib/joe_prices/contracts/v20/lb_router.ex
+++ apps/joe_prices/lib/joe_prices/contracts/v20/lb_router.ex
@@ -3,7 +3,7 @@ defmodule JoePrices.Contracts.V20.LbRouter do
     abi_file: "priv/abis/v20/LBRouter.json",
     default_address: "0xE3Ffc583dC176575eEA7FD9dF2A7c65F7E23f4C3"
 
-  def contract_for_network(:avalanche_mainner), do: "0xE3Ffc583dC176575eEA7FD9dF2A7c65F7E23f4C3"
-  def contract_for_network(:arbitrum_mainnet), do: "0x7BFd7192E76D950832c77BB412aaE841049D8D9B"
-  def contract_for_network(:bsc), do: "0xb66A2704a0dabC1660941628BE987B4418f7a9E8"
+  defp contract_for_network(:avalanche_mainner), do: "0xE3Ffc583dC176575eEA7FD9dF2A7c65F7E23f4C3"
+  defp contract_for_network(:arbitrum_mainnet), do: "0x7BFd7192E76D950832c77BB412aaE841049D8D9B"
+  defp contract_for_network(:bsc_mainnet), do: "0xb66A2704a0dabC1660941628BE987B4418f7a9E8"
 end
