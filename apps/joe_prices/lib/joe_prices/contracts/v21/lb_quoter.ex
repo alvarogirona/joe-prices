c5bd69ecb1cfc69af962492c5c55a655173aecfc
alvarogirona <alvaro.girona@gmail.com>
2023-08-18 18:42:20 +0200

    Remove unused modules from v2.1 contracts

--- apps/joe_prices/lib/joe_prices/contracts/v21/lb_quoter.ex
+++
@@ -1,4 +0,0 @@
-defmodule JoePrices.Contracts.V21.LbQuoter do
-  use Ethers.Contract,
-    abi_file: "priv/abis/v21/LBQuoter.json"
-end
--- apps/joe_prices/lib/joe_prices/contracts/v21/lb_router.ex
+++
@@ -1,4 +0,0 @@
-defmodule JoePrices.Contracts.V21.LbRouter do
-  use Ethers.Contract,
-    abi_file: "priv/abis/v21/LBRouter.json"
-end
