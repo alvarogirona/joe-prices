cbb47dfc1461bb5f44368ae5a92ebdc2c86689f9
alvarogirona <alvaro.girona@gmail.com>
2023-08-18 21:31:31 +0200

    Make contract_for_network private

--- apps/joe_prices/lib/joe_prices_v20.ex
+++ apps/joe_prices/lib/joe_prices_v20.ex
@@ -1,3 +1,9 @@
 defmodule JoePricesV20 do
-  
+  alias JoePrices.Boundary.V20.PriceRequest
+
+  def get_price(request = %PriceRequest{}) do
+  end
+
+  def get_price(network, request = %PriceRequest{}) do
+  end
 end
