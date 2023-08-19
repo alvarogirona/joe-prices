b7f9bc583a4c0669125ccfd15ab915b02c888c38
alvarogirona <alvaro.girona@gmail.com>
2023-08-19 13:41:58 +0200

    Create locustfile for testing and add sample tokens

---
+++ locust/locustfile.py
@@ -0,0 +1,30 @@
+import time
+from locust import HttpUser, task, between
+import json
+import random
+
+class JoeLocustTester(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def v21_simple(self):
+        pair = self.random_v21_pair()
+        tx = pair['token_x']
+        ty = pair['token_y']
+        bs = pair['bin_step']
+        self.client.get(f"/v2_1/prices/{tx}/{ty}/{bs}")
+
+    # @task
+    # def v21_batch(self):
+    #     self.client.post("/v21/", json={"username":"foo", "password":"bar"})
+
+    def random_v21_pair(self):
+        # Opening JSON file
+        f = open('v21_pairs.json')
+
+        data = json.load(f)
+
+        return random.choice(data)
+
+    def random_v21_batch(self):
+        pass
