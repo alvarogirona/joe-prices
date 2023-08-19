1f8572fc28431e5a64b84cb569a8084f3dff5fee
alvarogirona <alvaro.girona@gmail.com>
2023-08-19 19:38:10 +0200

    Serialize requests to the same pair for v21

--- apps/joe_prices/lib/joe_prices/boundary/v21/pair_repository.ex
+++ apps/joe_prices/lib/joe_prices/boundary/v21/pair_repository.ex
@@ -2,9 +2,74 @@ defmodule JoePrices.Boundary.V21.PairRepository do
   use GenServer
 
   alias JoePrices.Boundary.V21.PriceRequest
+  alias JoePrices.Boundary.V21.Cache.PriceCache
+  alias JoePrices.Boundary.V21.Cache.PriceCacheEntry
+  alias JoePrices.Core.V21.Pair
+
+  @bad_resp_addr "0x0000000000000000000000000000000000000000"
 
   def get_price(request = %PriceRequest{}) do
-    
+    with {:ok, pid} <- fetch_process(request) do
+      GenServer.call(pid, :fetch_price)
+    end
+  end
+
+  @doc """
+  ```
+  {:ok, pid} = JoePrices.Boundary.V21.PairRepository.fetch_process(price_request)
+  GenServer.call(pid, :fetch_price)
+  ```
+  """
+  def handle_call(:fetch_price, _from, request = %PriceRequest{}) do
+    pair_info = JoePrices.Boundary.V21.Cache.PriceCache.get_price(:avalanche_mainnet, request)
+    |> maybe_update_cache?(request, request.network)
+
+    {:reply, pair_info, request}
+  end
+
+  defp maybe_update_cache?({:ok, nil} = _resp, request = %PriceRequest{}, network) do
+    %{:token_x => tx, :token_y => ty, :bin_step => bin_step} = request
+
+    case JoePrices.Contracts.V21.LbFactory.fetch_pairs_for_tokens(network, tx, ty, bin_step) do
+      {:ok, pairs} ->
+        [info] = fetch_pairs_info(pairs, network: network)
+        PriceCache.update_prices(network, [info])
+        {:ok, info}
+      _ ->
+        {:error, "LBFactory contract call error (fetch_pairs_for_tokens)"}
+    end
+  end
+
+  defp maybe_update_cache?({:ok, value} = _resp, _request, _network) do
+    value
+  end
+
+  defp fetch_pairs_info(pairs, network: network) do
+    pairs
+    |> Enum.map(fn pair ->
+      case pair do
+        {_, @bad_resp_addr, _, _} ->
+          nil
+
+        {_, addr, _, _} ->
+          [token_x, token_y] = JoePrices.Contracts.V21.LbPair.fetch_tokens(network, addr)
+
+          {:ok, bin_step} = JoePrices.Contracts.V21.LbPair.fetch_bin_step(network, addr)
+          {:ok, [active_bin]} = JoePrices.Contracts.V21.LbPair.fetch_active_bin_id(network, addr)
+
+          %Pair{
+            name: "",
+            token_x: token_x,
+            token_y: token_y,
+            bin_step: bin_step,
+            active_bin: active_bin
+          }
+      end
+    end)
+  end
+
+  def init(args) do
+    {:ok, args}
   end
 
   def child_spec(request = %PriceRequest{}) do
@@ -32,7 +97,7 @@ defmodule JoePrices.Boundary.V21.PairRepository do
     }
   end
 
-  defp fetch_process(request = %PriceRequest{}) do
+  def fetch_process(request = %PriceRequest{}) do
     child_spec = child_spec(request)
 
     child = DynamicSupervisor.start_child(
--- apps/joe_prices/lib/joe_prices_v21.ex
+++ apps/joe_prices/lib/joe_prices_v21.ex
@@ -6,43 +6,15 @@ defmodule JoePricesV21 do
   alias JoePrices.Core.V21.Pair
   alias JoePrices.Utils.Parallel
 
-  @moduledoc """
-  Fecade for interacting with v2.1 pairs
-  """
-
-  @default_network Network.avalanche_mainnet()
-  @bad_resp_addr "0x0000000000000000000000000000000000000000"
-
-  @doc """
-  Get prices for the default network.
-  """
-  @spec get_prices([PriceRequest.t()]) :: [PriceCacheEntry.t()]
-  def get_prices(pairs) do
-    get_prices(@default_network, pairs)
-  end
-
   @doc """
   Get prices for a given network and tokens.
   """
-  @spec get_prices(atom, list(PriceRequest.t())) :: any
-  def get_prices(network, pairs) do
+  @spec get_prices(list(PriceRequest.t())) :: [PriceCacheEntry.t()]
+  def get_prices(pairs) do
     pairs
-    |> Parallel.pmap(fn request ->
-      get_price(network, request)
-    end)
+    |> Parallel.pmap(&get_price(&1))
     |> Enum.filter(&match?({:ok, _}, &1))
     |> Enum.map(fn {:ok, info} -> info end)
-  end
-
-  @doc """
-  Gets the price for a single pair and the default network.
-
-  ## Params
-  - `network`: `:avalanche_mainnet` | `:arbitrum_mainnet` | `:bsc_mainnnet`
-  """
-  @spec get_price(PriceRequest.t()) :: PriceCacheEntry.t()
-  def get_price(request = %PriceRequest{}) do
-    __MODULE__.get_price(@default_network, request)
   end
 
   @doc """
@@ -56,50 +28,8 @@ defmodule JoePricesV21 do
   iex> JoePricesV21.get_price(:avalanche_mainnet, request)
   ```
   """
-  @spec get_price(atom(), PriceRequest.t()) :: PriceCacheEntry.t()
-  def get_price(network, request = %PriceRequest{}) do
-    JoePrices.Boundary.V21.Cache.PriceCache.get_price(network, request)
-    |> maybe_update_cache?(request, @default_network)
-  end
-
-  defp maybe_update_cache?({:ok, nil} = _resp, request = %PriceRequest{}, network) do
-    %{:token_x => tx, :token_y => ty, :bin_step => bin_step} = request
-
-    case JoePrices.Contracts.V21.LbFactory.fetch_pairs_for_tokens(network, tx, ty, bin_step) do
-      {:ok, pairs} ->
-        [info] = fetch_pairs_info(pairs, network: network)
-        PriceCache.update_prices(network, [info])
-        {:ok, info}
-      _ ->
-        {:error, "LBFactory contract call error (fetch_pairs_for_tokens)"}
-    end
-  end
-
-  defp maybe_update_cache?({:ok, value} = _resp, _request, _network) do
-    value
-  end
-
-  defp fetch_pairs_info(pairs, network: network) do
-    pairs
-    |> Enum.map(fn pair ->
-      case pair do
-        {_, @bad_resp_addr, _, _} ->
-          nil
-
-        {_, addr, _, _} ->
-          [token_x, token_y] = JoePrices.Contracts.V21.LbPair.fetch_tokens(network, addr)
-
-          {:ok, bin_step} = JoePrices.Contracts.V21.LbPair.fetch_bin_step(network, addr)
-          {:ok, [active_bin]} = JoePrices.Contracts.V21.LbPair.fetch_active_bin_id(network, addr)
-
-          %Pair{
-            name: "",
-            token_x: token_x,
-            token_y: token_y,
-            bin_step: bin_step,
-            active_bin: active_bin
-          }
-      end
-    end)
+  @spec get_price(PriceRequest.t()) :: PriceCacheEntry.t()
+  def get_price(request = %PriceRequest{}) do
+    JoePrices.Boundary.V21.PairRepository.get_price(request)
   end
 end
