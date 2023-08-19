3ded471ae552e8f9bb7b00843094d4efca47fc91
alvarogirona <alvaro.girona@gmail.com>
2023-08-19 18:08:10 +0200

    Add network to PriceRequest
    - Defaults to `:avalanche_mainnet`

--- apps/joe_prices/lib/joe_prices/boundary/v21/price_request.ex
+++ apps/joe_prices/lib/joe_prices/boundary/v21/price_request.ex
@@ -1,15 +1,24 @@
 defmodule JoePrices.Boundary.V21.PriceRequest do
   @moduledoc """
   Struct definition for a v2.1 price request.
+
+  ```
+  alias JoePrices.Boundary.V21.PriceRequest
+  tx = "0x49d5c2bdffac6ce2bfdb6640f4f80f226bc10bab"
+  ty = "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e"
+  bs = 15
+  price_request = %PriceRequest{token_x: tx, token_y: ty, bin_step: bs}
   """
 
   defstruct token_x: "",
             token_y: "",
-            bin_step: 0
+            bin_step: 0,
+            network: :avalanche_mainnet
 
   @type t :: %__MODULE__{
           token_x: String.t(),
           token_y: String.t(),
-          bin_step: integer()
+          bin_step: integer(),
+          network: atom()
         }
 end
