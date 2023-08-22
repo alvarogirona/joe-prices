6f9025a551929a61dc97ea699291bcdff6e69b94
alvarogirona <alvaro.girona@gmail.com>
2023-08-22 13:22:16 +0200

    Fetch price from contract for joe v1

--- apps/joe_prices/lib/joe_prices/contracts/v1/joe_factory.ex
+++ apps/joe_prices/lib/joe_prices/contracts/v1/joe_factory.ex
@@ -12,10 +12,14 @@ defmodule JoePrices.Contracts.V1.JoeFactory do
 
     0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e
 
-    JoePrices.Contracts.V1.JoeFactory.get_pair("0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7", "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e")
+    JoePrices.Contracts.V1.JoeFactory.fetch_pair("0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7", "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e")
     """
+    @spec fetch_pair(String.t(), String.t(), atom()) :: {:ok, binary()} | {:error, any()}
+    def fetch_pair(token_x, token_y, network \\ :avalanche_mainnet) do
+      opts = Network.opts_for_call(network, "0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10")
 
-    def fetch_pair(token_x, token_y) do
-      opts = Network.opts_for_call(:avalanche_mainnet, "0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10")
+      with {:ok, [pair]} <- __MODULE__.get_pair(token_x, token_y, opts) do
+        {:ok, pair}
+      end
     end
 end
--- apps/joe_prices/lib/joe_prices/contracts/v1/joe_pair.ex
+++ apps/joe_prices/lib/joe_prices/contracts/v1/joe_pair.ex
@@ -1,4 +1,21 @@
 defmodule JoePrices.Contracts.V1.JoePair do
   use Ethers.Contract,
     abi_file: "priv/abis/v1/pair.json"
+
+  alias JoePrices.Core.Network
+
+  @doc """
+  ## Example
+
+  ```
+  iex> JoePrices.Contracts.V1.JoePair.fetch_price("0xf4003f4efbe8691b60249e6afbd307abe7758adb", :avalanche_mainnet)
+  ```
+  """
+  def fetch_price(pair, network) do
+    opts = Network.opts_for_call(network, pair)
+
+    with {:ok, [total, token_x, token_y]} <- __MODULE__.get_reserves(opts) do
+      token_x / token_y
+    end
+  end
 end
