efa535706bd9abfe731e80b336cdc5553fe51f89
alvarogirona <alvaro.girona@gmail.com>
2023-08-25 01:09:41 +0200

    Refactor v2 and v21 controllers

--- apps/joe_prices_web/lib/joe_prices_web/controllers/api/v21/price_controller.ex
+++ apps/joe_prices_web/lib/joe_prices_web/controllers/api/v21/price_controller.ex
@@ -5,42 +5,33 @@ defmodule JoePricesWeb.Api.V21.PriceController do
   alias JoePrices.Boundary.V2.PriceRequest
 
   def index(conn, opts) do
-    %{"bin_step" => bin_step, "token_x" => tx, "token_y" => ty} = opts
-
-    {bin_step, _} = Integer.parse(bin_step)
-
-    price_request = %PriceRequest{
-      token_x: tx,
-      token_y: ty,
-      bin_step: bin_step,
-      version: :v21
-    }
-
-    case JoePricesV2.get_price(price_request) do
-      {:ok, price} -> json(conn, render_price(price))
-      {:error, _} -> text(conn, "error")
-      price -> json(conn, render_price(price))
-    end
+    price_request = parse_token_request(opts)
+    price = JoePricesV2.get_price(price_request)
+    json(conn, render_price(price))
   end
 
   def batch(conn, %{"tokens" => tokens_list} = _opts) do
-    parsed_tokens = tokens_list
+    prices = tokens_list
     |> Enum.map(&parse_token_request/1)
-
-    prices = JoePricesV2.get_prices(parsed_tokens)
+    |> JoePricesV2.get_prices()
     |> render_prices()
 
     json(conn, prices)
   end
 
   defp parse_token_request(%{"token_x" => tx, "token_y" => ty, "bin_step" => bs} = _token_request) do
+    bin_step = parse_bin_step(bs)
+
     %PriceRequest{
       token_x: tx,
       token_y: ty,
-      bin_step: bs,
+      bin_step: bin_step,
       version: :v21
     }
   end
+
+  defp parse_bin_step(bs) when is_binary(bs), do: Integer.parse(bs) |> elem(0)
+  defp parse_bin_step(bs) when is_integer(bs), do: bs
 
   defp render_prices(prices) do
     Enum.map(prices, &render_price/1)
