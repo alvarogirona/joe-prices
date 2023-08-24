bf155d38165e4b252b609260da61e581b2bb56f3
alvarogirona <alvaro.girona@gmail.com>
2023-08-24 05:17:28 +0200

    Add network with default param to v1 PriceRequest

--- apps/joe_prices/lib/joe_prices/boundary/v1/price_request.ex
+++ apps/joe_prices/lib/joe_prices/boundary/v1/price_request.ex
@@ -1,4 +1,6 @@
 defmodule JoePrices.Boundary.V1.PriceRequest do
+  @type network_name :: :arbitrum_mainnet | :avalanche_mainnet | :bsc_mainnet
+
   @moduledoc """
   Struct definition for a v1 price request.
 
@@ -12,10 +14,13 @@ defmodule JoePrices.Boundary.V1.PriceRequest do
   iex> request2 = %JoePrices.Boundary.V1.PriceRequest{base_asset: "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e", quote_asset: "0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd"}
   ```
   """
-  defstruct [:base_asset, :quote_asset]
+  defstruct base_asset: "",
+    quote_asset: "",
+    network: :avalanche_mainnet
 
   @type t :: %__MODULE__{
     base_asset: String.t(),
-    quote_asset: String.t()
+    quote_asset: String.t(),
+    network: network_name()
   }
 end
