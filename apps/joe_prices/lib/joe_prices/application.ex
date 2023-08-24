7847d8a56be68111e6d00a9449678592d5622039
alvarogirona <alvaro.girona@gmail.com>
2023-08-24 04:26:49 +0200

    Spawn v1 caches on app supervisor

--- apps/joe_prices/lib/joe_prices/application.ex
+++ apps/joe_prices/lib/joe_prices/application.ex
@@ -39,14 +39,18 @@ defmodule JoePrices.Application do
 
     v21_caches =
       JoePrices.Core.Network.all_networks()
-      |> Enum.map(&v21_cache_child_from_network(&1))
+      |> Enum.map(&v21_cache_child_from_network/1)
 
     v20_caches =
       JoePrices.Core.Network.all_networks()
-      |> Enum.map(&v20_cache_child_from_network(&1))
+      |> Enum.map(&v20_cache_child_from_network/1)
+
+    v1_caches =
+      JoePrices.Core.Network.all_networks()
+      |> Enum.map(&v1_cache_child_from_network/1)
 
     opts = [strategy: :one_for_one, name: JoePrices.Supervisor]
-    Supervisor.start_link(children ++ v21_caches ++ v20_caches, opts)
+    Supervisor.start_link(children ++ v21_caches ++ v20_caches ++ v1_caches, opts)
   end
 
   defp v21_cache_child_from_network(network) do
@@ -63,6 +67,18 @@ defmodule JoePrices.Application do
 
   defp v20_cache_child_from_network(network) do
     cache_name = JoePrices.Boundary.V2.Cache.PriceCache.get_table_name(network, :v20)
+
+    Supervisor.child_spec(
+      {Cachex,
+       name: cache_name,
+       expiration: expiration(default: :timer.seconds(@cache_ttl_seconds)),
+       interval: nil},
+      id: cache_name
+    )
+  end
+
+  defp v1_cache_child_from_network(network) do
+    cache_name = JoePrices.Boundary.V1.Cache.PriceCache.get_table_name(network)
 
     Supervisor.child_spec(
       {Cachex,
