7fe18535ca852ed578b00fba5555104f729c2a43
alvarogirona <alvaro.girona@gmail.com>
2023-08-24 05:17:16 +0200

    Add tests to v1 PairRepository

--- apps/joe_prices/test/joe_prices/boundary/v1/pair_repository_test.exs
+++ apps/joe_prices/test/joe_prices/boundary/v1/pair_repository_test.exs
@@ -21,4 +21,21 @@ defmodule JoePrices.Boundary.V1.PairRepositoryTest do
 
     assert pid1 == pid2
   end
+
+  test "fetch_process returns different pids for requests with different token addresses" do
+    request1 = %PriceRequest{
+      base_asset: "0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd",
+      quote_asset: "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e"
+    }
+
+    request2 = %PriceRequest{
+      base_asset: "0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd",
+      quote_asset: "0xother_address"
+    }
+
+    {:ok, pid1} = PairRepository.fetch_process(request1)
+    {:ok, pid2} = PairRepository.fetch_process(request2)
+
+    refute pid1 == pid2
+  end
 end
