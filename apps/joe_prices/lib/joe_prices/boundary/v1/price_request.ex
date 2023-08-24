a1db937e181ddd0465462b47dfeb51e618516256
alvarogirona <alvaro.girona@gmail.com>
2023-08-24 04:20:28 +0200

    Add examples to v1 PriceRequest

--- apps/joe_prices/lib/joe_prices/boundary/v1/price_request.ex
+++ apps/joe_prices/lib/joe_prices/boundary/v1/price_request.ex
@@ -1,8 +1,17 @@
 defmodule JoePrices.Boundary.V1.PriceRequest do
   @moduledoc """
   Struct definition for a v1 price request.
-  """
 
+  ## Example
+
+  ```elixir
+  # Joe/USDC
+  iex> request = %JoePrices.Boundary.V1.PriceRequest{base_asset: "0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd", quote_asset: "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e"}
+
+  # USDC/Joe
+  iex> request = %JoePrices.Boundary.V1.PriceRequest{base_asset: "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e", quote_asset: "0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd"}
+  ```
+  """
   defstruct [:base_asset, :quote_asset]
 
   @type t :: %__MODULE__{
