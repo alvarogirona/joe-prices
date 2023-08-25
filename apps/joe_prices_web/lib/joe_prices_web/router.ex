ab273b71c1aca5e993a8afc91f1e5337629ea931
alvarogirona <alvaro.girona@gmail.com>
2023-08-25 16:27:31 +0200

    .

--- apps/joe_prices_web/lib/joe_prices_web/router.ex
+++ apps/joe_prices_web/lib/joe_prices_web/router.ex
@@ -36,7 +36,7 @@ defmodule JoePricesWeb.Router do
   #   pipe_through :api
   # end
 
-  # Enable LiveDashboard and Swoosh mailbox preview in development
+  # Enable LiveDashboard
   if Application.compile_env(:joe_prices_web, :dev_routes) do
     # If you want to use the LiveDashboard in production, you should put
     # it behind authentication and allow only admins to access it.
