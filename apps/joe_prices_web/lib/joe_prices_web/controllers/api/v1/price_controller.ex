181165cc1464bae97e3eb6d1554655ccccf74723
alvarogirona <alvaro.girona@gmail.com>
2023-08-25 13:13:52 +0200

    Refactor v1 price controller

--- apps/joe_prices_web/lib/joe_prices_web/controllers/api/v1/price_controller.ex
+++ apps/joe_prices_web/lib/joe_prices_web/controllers/api/v1/price_controller.ex
@@ -4,44 +4,39 @@ defmodule JoePricesWeb.Api.V1.PriceController do
   alias JoePrices.Contracts.V1.JoePair
   alias JoePrices.Boundary.V1.PriceRequest
 
-  def index(conn, %{"base_asset" => base_asset, "quote_asset" => quote_asset} = _params) do
-    price_request = %PriceRequest{
-      base_asset: base_asset,
-      quote_asset: quote_asset
-    }
+  def index(conn, params = %{"base_asset" => _, "quote_asset" => _}) do
+    price_request = parse_token_request(params)
 
     {:ok, joe_pair} = JoePricesV1.get_price(price_request)
 
     json(conn, %{
-      base_asset: base_asset,
-      quote_asset: quote_asset,
+      base_asset: price_request.base_asset,
+      quote_asset: price_request.quote_asset,
       price: joe_pair.price
     })
   end
 
-  def batch(conn, %{"tokens" => tokens_list} = _opts) do
-    parsed_requests = tokens_list
+  def batch(conn, %{"tokens" => tokens_list}) do
+    pairs = tokens_list
     |> Enum.map(&parse_token_request/1)
-
-    pairs = JoePricesV1.get_prices(parsed_requests)
-    |> render_pairs(tokens_list)
+    |> JoePricesV1.get_prices()
+    |> render_pairs()
 
     json(conn, pairs)
   end
 
-  defp parse_token_request(%{"base_asset" => base_asset, "quote_asset" => quote_asset} = _req) do
+  defp parse_token_request(%{"base_asset" => base_asset, "quote_asset" => quote_asset}) do
     %PriceRequest{
       base_asset: base_asset,
       quote_asset: quote_asset
     }
   end
 
-  @spec render_pairs(list(JoePair.t()), list()) :: any()
-  defp render_pairs(pairs, requests) do
-    Enum.map(pairs, fn pair_response ->
-      case pair_response do
-        {:ok, pair} -> render_ok_pair(pair)
-      end
+  @spec render_pairs(list(JoePair.t())) :: any()
+  defp render_pairs(pairs) do
+    Enum.map(pairs, fn
+      {:ok, pair} -> render_ok_pair(pair)
+      {:error, _} -> %{error: "could not fetch price"}
     end)
   end
 
