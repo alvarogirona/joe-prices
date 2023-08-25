e0cef22a44d848dcf33478ac39d3df8b2e15f676
alvarogirona <alvaro.girona@gmail.com>
2023-08-25 16:27:50 +0200

    Doctests for stable.ex

--- apps/joe_prices/lib/joe_prices/core/tokens/stable.ex
+++ apps/joe_prices/lib/joe_prices/core/tokens/stable.ex
@@ -1,15 +1,30 @@
 defmodule JoePrices.Core.Tokens.Stable do
+  @moduledoc """
+  Module to check if a given token for a network is an stable coin.
+  """
+
   alias JoePrices.Core.Tokens.USDC
   alias JoePrices.Core.Tokens.USDCe
   alias JoePrices.Core.Tokens.USDT
 
   @callback address_for_network(network :: atom()) :: String.t()
 
+  @doc """
+  Checks if a token is an stable coin.
+
+  ## Examples
+
+      iex> JoePrices.Core.Tokens.Stable.is_token_stable("0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e", :avalanche_mainnet)
+      true
+
+      iex> JoePrices.Core.Tokens.Stable.is_token_stable("0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6v", :avalanche_mainnet)
+      false
+  """
   def is_token_stable(token_address, network) do
     stable_coin_modules()
     |> Enum.map(fn module -> apply(module, :address_for_network, [network]) end)
     |> Enum.member?(token_address)
   end
 
-  def stable_coin_modules(), do: [USDC, USDT, USDCe]
+  defp stable_coin_modules(), do: [USDC, USDT, USDCe]
 end
---
+++ apps/joe_prices/test/joe_prices/core/tokens/stable_test.exs
@@ -0,0 +1,8 @@
+defmodule JoePrices.Core.Tokens.StableTest do
+  @moduledoc """
+  Tests for `JoePrices.Core.Tokens.Stable`
+  """
+
+  use ExUnit.Case, async: true
+  doctest JoePrices.Core.Tokens.Stable
+end
