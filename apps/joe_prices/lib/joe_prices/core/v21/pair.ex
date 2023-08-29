0084ea40178449854fb28535f63346671b3a25e1
alvarogirona <alvaro.girona@gmail.com>
2023-08-29 21:53:16 +0200

    Add address network and version field to Pair struct

--- apps/joe_prices/lib/joe_prices/core/v21/pair.ex
+++ apps/joe_prices/lib/joe_prices/core/v21/pair.ex
@@ -1,7 +1,13 @@
 defmodule JoePrices.Core.V21.Pair do
-  @enforce_keys [:name, :token_x, :token_y, :bin_step, :active_bin]
+  alias JoePrices.Core.Tokens.USDCe
+  alias JoePrices.Core.Tokens.USDT
+  alias JoePrices.Core.Tokens.USDC
+  alias JoePrices.Core.Tokens.Stable
 
-  defstruct name: "",
+  defstruct address: "",
+            network: :avalanche_mainnet,
+            version: :v21,
+            name: "",
             token_x: "",
             token_y: "",
             bin_step: 0,
@@ -9,6 +15,9 @@ defmodule JoePrices.Core.V21.Pair do
             price: 0
 
   @type t() :: %__MODULE__{
+          address: String.t(),
+          network: String.t(),
+          version: :v20 | :v21,
           name: String.t(),
           token_x: String.t(),
           token_y: String.t(),
@@ -16,4 +25,39 @@ defmodule JoePrices.Core.V21.Pair do
           active_bin: integer(),
           price: float()
         }
+
+  @avax_wavax_address "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7"
+  @arb_weth_address "0x82af49447d8a07e3bd95bd0d56f35241523fbab1"
+  @bsc_wbnb_address "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c"
+
+  def primary_quote_assets(:avalanche_mainnet = network) do
+    [
+      USDC.address_for_network(network),
+      USDT.address_for_network(network),
+      @avax_wavax_address
+    ]
+  end
+
+  def primary_quote_assets(:arbitrum_mainnet = network) do
+    [
+      USDC.address_for_network(network),
+      USDT.address_for_network(network),
+      USDCe.address_for_network(network),
+      @arb_weth_address
+    ]
+  end
+
+  def primary_quote_assets(:bsc_mainnet = network) do
+    [
+      USDT.address_for_network(network),
+      @bsc_wbnb_address
+    ]
+  end
+
+  def is_primary_quote_asset?(token_address, network) do
+    downcased_token = String.downcase(token_address)
+
+    primary_quote_assets(network)
+    |> Enum.member?(downcased_token)
+  end
 end
