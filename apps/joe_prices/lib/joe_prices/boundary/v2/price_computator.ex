777110db8d7b0c0b04c716ccaa531658409cfc98
alvarogirona <alvaro.girona@gmail.com>
2023-08-31 16:55:40 +0200

    Refactor price computator to remove dup code
    - Extract common code of compute_price_with_primary_quote_asset to `compute_price_with_related_stable` method

--- apps/joe_prices/lib/joe_prices/boundary/v2/price_computator.ex
+++ apps/joe_prices/lib/joe_prices/boundary/v2/price_computator.ex
@@ -8,6 +8,7 @@ defmodule JoePrices.Boundary.V2.PriceComputator do
   alias JoePrices.Core.V2.Token
   alias JoePrices.Contracts.V21.LbPair
   alias JoePrices.Boundary.V2.PairRepository
+  alias JoePrices.Boundary.V2.PairInfoCache.PairCacheEntry
   alias JoePrices.Boundary.V2.PairInfoCache.PairsInfoFetcher
   alias JoePrices.Core.Tokens.Stable
   alias JoePrices.Boundary.V2.PriceRequest
@@ -19,10 +20,14 @@ defmodule JoePrices.Boundary.V2.PriceComputator do
     cond do
       has_stable_in_tokens(request) ->
         compute_x_div_y_price(request, active_bin)
-      has_primary_quote_asset(request) -> # Example BTC.b/Avax
+
+      # Example BTC.b/Avax
+      has_primary_quote_asset(request) ->
         compute_price_with_primary_quote_asset(request, pair_addr, active_bin)
-      true -> # Does not have a primary quote asset. Example: WETH/BTC.b in Avalanche
-        -7 # TODO
+
+      # Does not have a primary quote asset. Example: WETH/BTC.b in Avalanche
+      true ->
+        compute_price_without_primary_quote_asset(request, pair_addr, active_bin)
     end
   end
 
@@ -33,11 +38,11 @@ defmodule JoePrices.Boundary.V2.PriceComputator do
     raw_price = JoePrices.Core.V2.Bin.get_price_from_id(active_bin, request.bin_step)
     price_multiplier = :math.pow(10, token_x_decimals - token_y_decimals)
 
-    if request.token_x < request.token_y do
-      raw_price * price_multiplier
-    else
-      1 / raw_price * price_multiplier
-    end
+    # if request.token_x < request.token_y do
+    raw_price * price_multiplier
+    # else
+    #   (1 / raw_price) * price_multiplier
+    # end
   end
 
   defp compute_price_with_primary_quote_asset(request = %PriceRequest{}, pair_addr, active_bin) do
@@ -60,63 +65,74 @@ defmodule JoePrices.Boundary.V2.PriceComputator do
     case {stable_pairs_token_x, stable_pairs_token_y} do
       {[], []} ->
         -1
-
       {[stable_related_pair | _token_x_pairs], []} ->
-        request_for_x = %PriceRequest{
-          token_x: stable_related_pair.token_x,
-          token_y: stable_related_pair.token_y,
-          bin_step: stable_related_pair.bin_step,
-          network: request.network,
-          version: request.version
-        }
-
-        %Pair{price: price_x_in_dollars} =
-          p_info = PairRepository.fetch_pair_info(stable_related_pair.pair_address, request_for_x)
-
-        price = compute_x_div_y_price(request, active_bin)
-        price_y_in_dollars = price_x_in_dollars / price
-
-        has_enough_liquidity? = LbPair.pair_has_enough_reserves_around_active_bin?(token_x, token_y, pair_addr, active_bin, request.network, price_x_in_dollars, price_y_in_dollars)
-
-        if has_enough_liquidity? do
-          price
-        else
-          -1
-        end
+        compute_price_with_related_stable(request, stable_related_pair, pair_addr, active_bin)
       {[], [stable_related_pair | _token_y_pairs]} ->
-        request_for_y = %PriceRequest{
-          token_x: stable_related_pair.token_x,
-          token_y: stable_related_pair.token_y,
-          bin_step: stable_related_pair.bin_step,
-          network: request.network,
-          version: request.version
-        }
-
-        p_info = PairRepository.fetch_pair_info(stable_related_pair.pair_address, request_for_y)
-
-        price = compute_x_div_y_price(request, active_bin)
+        compute_price_with_related_stable(request, stable_related_pair, pair_addr, active_bin)
       {[stable_pair_x | _], [_stable_pair_y | _]} ->
-        request_for_x = %PriceRequest{
-          token_x: stable_pair_x.token_x,
-          token_y: stable_pair_x.token_y,
-          bin_step: stable_pair_x.bin_step,
-          network: request.network,
-          version: request.version
-        }
+        compute_price_with_related_stable(request, stable_pair_x, pair_addr, active_bin)
+    end
+  end
 
-        %Pair{price: price_x_in_dollars} =
-          p_info = PairRepository.fetch_pair_info(stable_pair_x.pair_address, request_for_x)
+  defp compute_price_with_related_stable(
+         %PriceRequest{token_x: token_x, token_y: token_y} = request,
+         %PairCacheEntry{} = stable_related_pair,
+         pair_addr,
+         active_bin
+       ) do
+    request_for_related = %PriceRequest{
+      token_x: stable_related_pair.token_x,
+      token_y: stable_related_pair.token_y,
+      bin_step: stable_related_pair.bin_step,
+      network: request.network,
+      version: request.version
+    }
 
-        price = compute_x_div_y_price(request, active_bin)
-        price_y_in_dollars = price_x_in_dollars / price
+    %Pair{price: price_related_in_dollars} =
+      p_info =
+      PairRepository.fetch_pair_info(stable_related_pair.pair_address, request_for_related)
 
-        has_enough_liquidity? = LbPair.pair_has_enough_reserves_around_active_bin?(token_x, token_y, pair_addr, active_bin, request.network, price_x_in_dollars, price_y_in_dollars)
+    price = compute_x_div_y_price(request, active_bin)
+    price_y_in_dollars = price_related_in_dollars / price
 
-        if has_enough_liquidity? do
-          price
-        else
-          -1
-        end
+    has_enough_liquidity? =
+      LbPair.pair_has_enough_reserves_around_active_bin?(
+        token_x,
+        token_y,
+        pair_addr,
+        active_bin,
+        request.network,
+        price_related_in_dollars,
+        price_y_in_dollars
+      )
+
+    if has_enough_liquidity? do
+      price
+    else
+      -1
+    end
+  end
+
+  defp compute_price_without_primary_quote_asset(request = %PriceRequest{token_x: token_x, token_y: token_y}, pair_addr, active_bin) do
+    stable_pairs_token_x =
+      PairsInfoFetcher.find_stable_pairs_with_token(
+        token_x,
+        request.version,
+        request.network
+      )
+
+    stable_pairs_token_y =
+      PairsInfoFetcher.find_stable_pairs_with_token(
+        token_y,
+        request.version,
+        request.network
+      )
+
+    case {stable_pairs_token_x, stable_pairs_token_y} do
+      {[], []} -> -1
+      {[token_x_related | _], []} -> -2
+      {[], [token_y_related | _]} -> -3
+      {[token_x_related | _], [token_y_related | _]} -> -4
     end
   end
 
